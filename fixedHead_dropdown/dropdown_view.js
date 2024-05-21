f44fcacc41de30b5a06a4fca06732e70d46bbc87
Thambiraj V <thambiraj-21355@thambi-pt7283.csez.zohocorpin.com>
2024-05-21 19:28:28 +0530

    before backup

--- fixedHead_dropdown/dropdown_view.js
+++ fixedHead_dropdown/dropdown_view.js
@@ -1,7 +1,6 @@
 
 var dropdown_component = {
     template:`
-<<<<<<< HEAD
     <div class="dropdown_root" >
         <div id="dropdown_head" @click = change_activestate :class="active == name ? 'active' : '' " ref="dropdown">
             <span :class = "icon ?'material-symbols-outlined':'hide'">{{icon}}</span>
@@ -10,16 +9,6 @@ var dropdown_component = {
         </div>
         <div id="dropdown_body" :class="active ? active != name ? 'hide':'':''" ref="dropdown" v-if="show_dropdown">
             <div class="dropdown-item" v-for="value in  dropdown_data" :class="{ selected : value === default_selected}" @click="change_value(value)">{{value}}</div>
-=======
-    <div class="dropdown_root">
-        <div id="dropdown_head" @click = change_activestate :class="active == id ? 'active' : '' ">
-            <span :class = "first_icon ?'material-symbols-outlined':'hide'">{{first_icon}}</span>
-            <span>{{title}}</span>
-            <span class="material-symbols-outlined drop_down"> arrow_drop_down </span>
-        </div>
-        <div id="dropdown_body" :class="active ? active !== id ? 'hide':'':''" ref="dropdown" >
-            <div class="dropdown-item" v-for="value in data" :class="{ selected : value === default_selected}" @click="change_value(value)">{{value}}</div>
->>>>>>> parent of ddd91e1 (for updating  before backup)
         </div>
     </div>`,
     props:{
@@ -29,13 +18,8 @@ var dropdown_component = {
         data:{
             type: Array,
         },
-<<<<<<< HEAD
         name:{
             type:[Number,String],
-=======
-        title:{
-            type:String,
->>>>>>> parent of ddd91e1 (for updating  before backup)
         },
         icon:{
             type:String
@@ -54,7 +38,6 @@ var dropdown_component = {
     data(){
         return {
             selected: this.default_selected,
-<<<<<<< HEAD
             show_dropdown:true,
             dropdown_icon:this.active  == this.id ? "arrow_drop_up":'arrow_drop_down',
         }
@@ -68,9 +51,6 @@ var dropdown_component = {
             else{
                 this.dropdown_icon =  "arrow_drop_down";
             }
-=======
-            show_dropdown:true
->>>>>>> parent of ddd91e1 (for updating  before backup)
         }
     },
     methods:{
@@ -80,7 +60,6 @@ var dropdown_component = {
             // this.$emit('change_value',this.selected);
         },
         change_activestate(){
-<<<<<<< HEAD
             // this.$emit('change_activestate',this.id);
             this.root_ref.eventbus.change_head(this.name);
             this.root_ref.eventbus.change_value(this.dropdown_data[0]);
@@ -88,10 +67,6 @@ var dropdown_component = {
             this.root_ref.eventbus.get_dropdown_values(this.name);
             this.show_dropdown = !this.show_dropdown;
             this.dropdown_icon = this.dropdown_icon=="arrow_drop_up" ?"arrow_drop_down":"arrow_drop_up";
-=======
-            this.$emit('change_activestate',this.id);
-            this.show_dropdown = !this.show_dropdown
->>>>>>> parent of ddd91e1 (for updating  before backup)
         }
     }
 }
